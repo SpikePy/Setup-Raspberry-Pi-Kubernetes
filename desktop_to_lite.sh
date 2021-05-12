b4db016e20fece5bc29eb1bc5d470448fffb8eb6
SpikePy <hoffelmann@gmail.com>
2021-05-12 07:21:24 +0200

    Create desktop_to_lite.sh

---
+++ desktop_to_lite.sh
@@ -0,0 +1,13 @@
+#!/bin/dash
+
+##################################################################################
+#                                                                                #
+# Make raspian desktop varian (only variant available for arm64) to lite variant #
+#                                                                                #
+##################################################################################
+
+sudo apt update
+sudo apt purge -y x11-common bluez gnome-menus gnome-icon-theme gnome-themes-standard
+sudo apt purge -y hicolor-icon-theme gnome-themes-extra-data bluealsa cifs-utils
+sudo apt purge -y desktop-base desktop-file-utils
+sudo apt autoremove -y
